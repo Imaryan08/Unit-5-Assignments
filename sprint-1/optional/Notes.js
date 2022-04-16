78aedcc2d261630edca49d4efb90d422d1dc9559
Rampukar <imaryan08@gmail.com>
2022-04-16 22:25:58 +0530

    lecture links

---
+++ sprint-1/optional/Notes.js
@@ -0,0 +1,25 @@
+Day2 ;
+LiveClass = "https://masai-course.s3.ap-south-1.amazonaws.com/material/videos/26327/gTpUXrWuaqALE7BY4mv3pwkOU3YIzMk061zgSvRz.mp4"
+PreClass1 = "https://masai-course.s3.ap-south-1.amazonaws.com/material/videos/26291/8AJzDWj8EhcLDMMU9YZDBwMgvDE6SXdc9OS57Tr4.mp4"
+PreClass = "https://masai-course.s3.ap-south-1.amazonaws.com/material/videos/26289/Zv56goTb4g6gfK4m8dbLNIODsXdo0PaBv0LhWSeQ.mp4"
+
+Day3 ;
+LiveClass = "https://masai-course.s3.ap-south-1.amazonaws.com/material/videos/26376/DaTkVWb34RNJK02zTZt8tD8pdkJH0UTAwCJMMIIh.mp4";
+PreClass = "https://masai-course.s3.ap-south-1.amazonaws.com/material/videos/26351/1KMZFplwe37UEsEX2FCysvETGXiSOilN3fI8HrPq.mov"
+
+Day4 ;
+LiveClass = "https://masai-course.s3.ap-south-1.amazonaws.com/material/videos/26449/PMEqdUChCgPUUKg5pU9Lw8NSC6GLNzp91yz2NzFN.mp4";
+PreClass = "https://us06web.zoom.us/rec/share/LBbpUV2BoVeLp_LqIv35Sr6f2bbEpCLWhIs5SeteIUxwroh302fJQUyre4mYgBwq.96n6Qzu8y23VxwmJ";
+
+Day5 ;
+LiveClass = "https://masai-course.s3.ap-south-1.amazonaws.com/material/videos/26476/DGZEW7kwwaeHtNPRhQHYQER5mv1N4iEFTmCoYvaZ.mov";
+PreClass = "https://masai-course.s3.ap-south-1.amazonaws.com/material/videos/26496/PHq2ePM900kmHd9xCjBkHmeGIfGwpPs1JZi4XdNe.mp4";
+
+Day6 ;
+LiveClass = "";
+PreClass = "https://masai-course.s3.ap-south-1.amazonaws.com/material/videos/26564/LnzZ9Ln9ku55ZIRnWuJtCHFoMXNVFeC2L7UBwcFf.mp4";
+
+Day7 ;
+LiveClass = "https://masai-course.s3.ap-south-1.amazonaws.com/material/videos/26602/nYaDQ4ABmAPpqWWx2bg6kCOHEQuXNLQTvAzliven.mp4";
+PreClass = "https://masai-course.s3.ap-south-1.amazonaws.com/material/videos/26615/mihpoPkcp6Ce2OxcsQr368EagakRumGoffL6wOuk.mp4";
+
